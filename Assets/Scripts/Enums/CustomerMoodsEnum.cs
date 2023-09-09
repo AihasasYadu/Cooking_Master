59d687ac5154c88142de367f26d4193cd2d99061
Aihasas Yadu <yadu.aihasas@gmail.com>
2023-09-10 03:49:18 +0530

    -->VOs and Enums added for Level Data class

---
+++ Assets/Scripts/Enums/CustomerMoodsEnum.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Enum
+{
+    public enum CustomerMoodsEnum
+    {
+        NONE = -1,
+        HAPPY,
+        ANGRY
+    }
+}
---
+++ Assets/Scripts/VOs/CustomerVO.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Scripts.Enum;
+using UnityEngine;
+
+namespace Scripts.VO
+{
+    public class CustomerVO
+    {
+        [SerializeField]
+        private int _customerId = 0;
+        public int CustomerId
+        {
+            get
+            {
+                return _customerId;
+            }
+
+            set
+            {
+                _customerId = value;
+            }
+        }
+
+        [SerializeField]
+        private CustomerMoodsEnum _customerMood = CustomerMoodsEnum.NONE;
+        public CustomerMoodsEnum CustomerMood
+        {
+            get
+            {
+                return _customerMood;
+            }
+
+            set
+            {
+                _customerMood = value;
+            }
+        }
+
+        [SerializeField]
+        private string _customerOrder = string.Empty;
+        public string CustomerOrder
+        {
+            get
+            {
+                return _customerOrder;
+            }
+
+            set
+            {
+                _customerOrder = value;
+            }
+        }
+    }
+}
